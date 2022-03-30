396e55ba5147b9f819b80380fdc98e37dabbdec2
Maestro28 <bohdan.rybak28@gmail.com>
2022-03-30 20:34:44 +0300

    all books page

--- book/views.py
+++ book/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from book.models import Book
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'book/index.html')
+
+def allbooks(request):
+    book_objects = Book.objects.all()
+    context = {'book_objects': book_objects}
+    return render(request, 'book/allbooks.html', context)
