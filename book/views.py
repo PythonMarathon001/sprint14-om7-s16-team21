caf8f13bf866f4505fe21694d84d6d4026dc2c8c
Metelskiy <97842940+Metelskiy@users.noreply.github.com>
2022-03-31 15:51:38 +0300

    test1

--- book/views.py
+++ book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+from book.models import Book
+def home(request):
+    return HttpResponse('home')
+def show_all(request):
+    #return HttpResponse(print(Book.get_all()))
+    #return HttpResponse(Book.objects.all())
+    return HttpResponse(Book.objects.get)
